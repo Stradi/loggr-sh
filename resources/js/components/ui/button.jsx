21853bf56fca50b333240c1645cbb218a43433f3
Batın Evirgen <batinevirgen@gmail.com>
2023-06-06 20:32:09 +0300

    feat: add verify email page

--- resources/js/components/ui/button.jsx
+++ resources/js/components/ui/button.jsx
@@ -1,14 +1,17 @@
 import clsx from "clsx";
 
-export default function Button({ className, ...props }) {
+export default function Button({ className, disabled, ...props }) {
   return (
     <button
       className={clsx(
         "p-2 flex items-center justify-center leading-loose text-center rounded-xl font-semibold text-neutral-100",
         "transition-[box-shadow,background-color] duration-150",
         "bg-neutral-900 hover:bg-neutral-800",
-        "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-neutral-600"
+        "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-neutral-600",
+        disabled && "opacity-75 cursor-not-allowed hover:bg-neutral-900",
+        className
       )}
+      disabled={disabled}
       {...props}
     />
   );
--- resources/js/components/ui/input.jsx
+++ resources/js/components/ui/input.jsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 
-export default function Input({ className, ...props }) {
+export default function Input({ className, disabled, ...props }) {
   return (
     <input
       {...props}
@@ -9,8 +9,11 @@ export default function Input({ className, ...props }) {
         "transition-[box-shadow] duration-150 text-neutral-800",
         "hover:ring-neutral-600",
         "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-neutral-600",
+        disabled &&
+          "opacity-75 cursor-not-allowed ring-neutral-200 hover:ring-neutral-200 text-neutral-500",
         className
       )}
+      disabled={disabled}
     />
   );
 }
